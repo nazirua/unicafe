c7e44375879ccd5ab17fc40cdb2e4ea1b7c3d417
Naziru Abdussalam Ibrahim <naziruabdussalami@gmail.com>
2022-01-29 10:53:57 +0000

    Add unicafe step1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+// import './App.css';
+
+
+const Button = ({onClick, text}) => <button onClick={onClick}> {text} </button>
+  
+
 
 function App() {
+   // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+
+  const goodSet = () => setGood(good+1);
+  const badSet = () => setBad(bad+1);
+  const neutralSet = () => setNeutral(neutral+1);
+
+  console.log({good})
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Give feedback</h1>
+      <Button 
+       onClick={goodSet} text='Good' />
+      <Button 
+       onClick={neutralSet} text='Neutral' />
+       <Button 
+       onClick={badSet} text='Bad' />
+
+       <h2>Statistics:</h2>
+
+       <div><p>Good {good} </p></div>
+       <div><p>Neutral {neutral} </p></div>
+       <div><p>Bad {bad} </p></div>
+
     </div>
   );
 }
