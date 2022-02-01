8d149f0f1a00a54b95809f668e1548e324f7618e
Naziru Abdussalam Ibrahim <naziruabdussalami@gmail.com>
2022-02-01 06:03:08 +0000

    Add logic

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React, {useState} from 'react';
-// import './App.css';
+import React, {useState} from 'react'
       
+// statistics component
 const Statistics =(props) => {
-  if(props.good <= 0){
+  if(props.good === 0 && props.bad === 0 && props.neutral === 0 ){
     return(
       <div>No feedback given</div>
-      )
+    )
   }
+  else{
     return(
       <div>{props.text} {props.value}</div>
     )
+  }
 }
-
+// Button component
 const Button = ({onClick, text}) => <button onClick={onClick}> {text} </button>
   
+
 
 
 function App() {
@@ -24,8 +27,6 @@ function App() {
   const goodSet = () => setGood(good+1);
   const badSet = () => setBad(bad+1);
   const neutralSet = () => setNeutral(neutral+1);
-
-  
 
   return (
     <div className="App">
@@ -38,14 +39,14 @@ function App() {
        onClick={badSet} text='Bad' />
 
       <h2>statistics</h2>
+  
       <Statistics text='Good' value={good} />
-      {/* <Statistics text='Neutral' value={neutral} />
+      <Statistics text='Neutral' value={neutral} />
       <Statistics text='Bad' value={bad} />
       <Statistics text='All' value={good+bad+neutral} />
       <Statistics text='Average' value={(good-bad)/(good + bad + neutral)} />
-      <Statistics text='Percentage' value={(good/(good + bad + neutral))*100} /> */}
-
-
+      <Statistics text='Percentage' value={(good/(good + bad + neutral))*100} />
+     
     </div>
   );
 }
